bd0a2fe3ad5aff5dc87e670f33656fd073733d81
Matvey Melnikov <55649891+MatveyMelnikov@users.noreply.github.com>
2025-01-01 18:23:11 +0300

    Implemented work with the status register. Sleep functions have been removed (#6)

--- Modules/CY15B104Q_Driver/Inc/cy15b104q_driver.h
+++ Modules/CY15B104Q_Driver/Inc/cy15b104q_driver.h
@@ -10,9 +10,17 @@ void cy15b104q_driver_deinit_module(void);
 cy15b104q_driver_status cy15b104q_driver_power_up(void);
 cy15b104q_driver_status cy15b104q_driver_check_link(void);
 cy15b104q_driver_status cy15b104q_driver_read_id(
-  cy15b104q_driver_id *const result
+  cy15b104q_driver_id *const result_id
 );
-cy15b104q_driver_status cy15b104q_driver_sleep(void);
-cy15b104q_driver_status cy15b104q_driver_sleep_recover(void);
+cy15b104q_driver_status cy15b104q_driver_read_status_register(
+  uint8_t *const result_status_register
+);
+cy15b104q_driver_status cy15b104q_driver_write_status_register(
+  bool write_protect_enabled,
+  bool block_protect_0,
+  bool block_protect_1
+);
+cy15b104q_driver_status cy15b104q_driver_write_enable(void);
+cy15b104q_driver_status cy15b104q_driver_write_disable(void);
 
 #endif
